1713d620e60aee8d660e4084f70c67fe930da33b
Tor Vic <torvic9@mailbox.org>
2022-06-20 12:40:39 +0200

    mm: Change dirty writeback defaults
    Use a lower ratio of 5, but a longer period of 10 seconds for dirty writeback.

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -71,7 +71,7 @@ static long ratelimit_pages = 32;
 /*
  * Start background writeback (via writeback threads) at this percentage
  */
-static int dirty_background_ratio = 10;
+static int dirty_background_ratio = 5;
 
 /*
  * dirty_background_bytes starts at 0 (disabled) so that it is a function of
@@ -99,7 +99,7 @@ static unsigned long vm_dirty_bytes;
 /*
  * The interval between `kupdate'-style writebacks
  */
-unsigned int dirty_writeback_interval = 5 * 100; /* centiseconds */
+unsigned int dirty_writeback_interval = 10 * 100; /* centiseconds */
 
 EXPORT_SYMBOL_GPL(dirty_writeback_interval);
 
