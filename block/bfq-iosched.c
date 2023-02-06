696e04feffb02747db23987e8e2df9501c78957a
Piotr Gorski <lucjan.lucjanov@gmail.com>
2023-02-06 09:55:46 +0100

    Revert "block, bfq: fix uaf for bfqq in bic_set_bfqq()"
    This reverts commit 9c72a7356af7498e934f5ea72c38a9bea858a500.
    
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -5425,11 +5425,9 @@ static void bfq_check_ioprio_change(struct bfq_io_cq *bic, struct bio *bio)
 
 	bfqq = bic_to_bfqq(bic, false);
 	if (bfqq) {
-		struct bfq_queue *old_bfqq = bfqq;
-
+		bfq_release_process_ref(bfqd, bfqq);
 		bfqq = bfq_get_queue(bfqd, bio, false, bic, true);
 		bic_set_bfqq(bic, bfqq, false);
-		bfq_release_process_ref(bfqd, old_bfqq);
 	}
 
 	bfqq = bic_to_bfqq(bic, true);
